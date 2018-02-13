ce94bccca0438156cb7a0395aff881dedc307a7e
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-02-13 01:00:08 -0200

    follow back function

---
+++ follow_back.py
@@ -0,0 +1,38 @@
+# Follow back
+
+from twython import Twython, TwythonError
+import time
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+twitter = Twython(consumer_key,
+                  consumer_secret,
+                  access_token,
+                  access_token_secret)
+
+# Define a static followers list
+followers = []
+
+def followBack(screen_name):
+    while true:
+        try:
+            ids = twitter.get_followers_ids(screen_name = screen_name)
+            for id in ids:
+                if id in followers:
+                    continue
+                else:
+                    twitter.create_friendship(user_id = id)
+                    followers.append(id)
+            
+        except TwythonError as e:
+            pass
+
+        # Fazer uma vez por dia
+        time.sleep(86400)
+
+# Action!
+followBack('datascienceimih')
